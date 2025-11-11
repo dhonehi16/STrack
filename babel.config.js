2436f2a755ea6fca48fc0015b66f99f50dea1a7b
dhonehi <dhonehi@gmail.com>
2025-11-11 22:16:42 +0300

    save

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@': './src',
+          '@/api': './src/api',
+          '@/components': './src/components',
+          '@/navigation': './src/navigation',
+          '@/types': './src/types',
+          '@/utils': './src/utils',
+        },
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    ],
+  ],
 };
--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
 
 /**
  * Metro configuration
@@ -6,6 +7,17 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@/api': path.resolve(__dirname, 'src/api'),
+      '@/components': path.resolve(__dirname, 'src/components'),
+      '@/navigation': path.resolve(__dirname, 'src/navigation'),
+      '@/types': path.resolve(__dirname, 'src/types'),
+      '@/utils': path.resolve(__dirname, 'src/utils'),
+    },
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
